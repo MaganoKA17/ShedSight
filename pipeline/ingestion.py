d75d5305a689eaf5680044ed6683dfb3ac58b3e1
Kgosi-E-tsile Magano <kgmagjhb025@student.wethinkcode.co.za>
2026-06-19 09:33:23 +0200

    feat: added method that saves raw APi response to database

--- pipeline/ingestion.py
+++ pipeline/ingestion.py
@@ -10,3 +10,13 @@ HEADERS = {"Token" : ESKOMPUSH_API_KEY}
 def get_area_schedule(area_id):
     response = requests.get(f"{BASE_URL}/area", headers=HEADERS, params= {"id": area_id})
     return response.json()
+
+def save_raw_outage(area_id, area_name, stage, schedule):
+    data = {
+        "area_id" : area_id,
+        "area_name" : area_name,
+        "stage" : stage,
+        "schedule" : schedule
+    }
+    supabase.table("raw_outages").insert(data).execute()
+    print(f"Saved raw data for {area_name}")
