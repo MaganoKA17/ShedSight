2459b894cfa99dcd044675c7b26d9a5975cbe9b5
Kgosi-E-tsile Magano <kgmagjhb025@student.wethinkcode.co.za>
2026-06-19 09:28:41 +0200

    feat: added method that gets area schedule via EskomAPI

--- pipeline/ingestion.py
+++ pipeline/ingestion.py
@@ -2,4 +2,11 @@ import requests
 from supabase import create_client
 from config import SUPABASE_URL, SUPABASE_KEY, ESKOMPUSH_API_KEY
 
-supabse = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+BASE_URL = "https://developer.sepush.co.za/business/2.0"
+HEADERS = {"Token" : ESKOMPUSH_API_KEY}
+
+def get_area_schedule(area_id):
+    response = requests.get(f"{BASE_URL}/area", headers=HEADERS, params= {"id": area_id})
+    return response.json()
