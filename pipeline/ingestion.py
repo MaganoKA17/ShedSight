f7e33680eed53a70675800c94149b909f57e98b2
Kgosi-E-tsile Magano <kgmagjhb025@student.wethinkcode.co.za>
2026-06-22 15:12:44 +0200

    refactor: added weekly .csv records to database

--- pipeline/ingestion.py
+++ pipeline/ingestion.py
@@ -25,17 +25,20 @@ def load_weekly_outages(filepath):
         for row in reader:
             data = {
                 "week_date" : row["Week_min_DateKey"],
-                "min_uclf_oclf" : float(row["Min of UCLF + OCLF"]),
-                "avg_uclf_oclf" : float(row["Average of UCLF + OCLF"]),
-                "max_uclf_oclf" : float(row["Max of UCLF + OCLF"]),
+                "min_uclf_oclf" : float(row["Min of UCLF+OCLF"]),
+                "avg_uclf_oclf" : float(row["Average of UCLF+OCLF"]),
+                "max_uclf_oclf" : float(row["Max of UCLF+OCLF"]),
             }
             supabase.table("raw_weekly_outages").insert(data).execute()
             print(f"Inserted row for {row['Week_min_DateKey']}")
 
 if __name__ == "__main__":
-    load_hourly_outages("hourly_outages.csv")
+    # load_hourly_outages("hourly_outages.csv")
     load_weekly_outages("weekly_outages.csv")
     # result = supabase.table("raw_hourly_outages").select("*").execute()
     # print(f"Connection test: {result}")
     # print(f"Supabase URL: {SUPABASE_URL}")
     # print(f"Supabase Key: {SUPABASE_KEY[:20]}...")
+    # with open("weekly_outages.csv", "r") as f:
+    #     reader = csv.DictReader(f)
+    #     print(reader.fieldnames)
