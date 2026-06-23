7a036f799831049649dcf85e789114fd7813cf73
Kgosi-E-tsile Magano <kgmagjhb025@student.wethinkcode.co.za>
2026-06-23 11:36:04 +0200

    refactor: edited transform.py code in order to use the python pandas library

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,43 +1,42 @@
+import pandas as pd
 from supabase import create_client
 from config import SUPABASE_URL, SUPABASE_KEY
-from datetime import datetime
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def transform_hourly_to_daily():
-    print("Fetching raw hourly data")
+   
+    print("Fetching raw hourly data from Supabase...")
     result = supabase.table("raw_hourly_outages").select("*").execute()
-    rows = result.data
 
-    daily_data = {}
+    df = pd.DataFrame(result.data)
 
-    for row in rows:
-        date = row["datetime_hour_beginning"][:10]
+    df["datetime_hour_beginning"] = pd.to_datetime(df["datetime_hour_beginning"])
+    df["hourly_uclf_oclf"] = pd.to_numeric(df["hourly_uclf_oclf"], errors="coerce")
+    df.dropna()
 
-        if date not in daily_data:
-            daily_data[date] = {
-                "uclf_oclf_values" : []
-            }
-        daily_data[date]["uclf_oclf_values"].append(float(row["hourly_uclf_oclf"]))
-    
-    print("Aggregatting daily metrics...")
+    df["date"] = df["datetime_hour_beginning"].dt.date
 
-    for date, values in daily_data.items():
-        uclf_values = values["uclf_oclf_values"]
-        avg = sum(uclf_values) / len(uclf_values)
-        maximum = max(uclf_values)
+    daily = df.groupby("date").agg(
+        avg_uclf_oclf=("hourly_uclf_oclf", "mean"),
+        max_uclf_oclf=("hourly_uclf_oclf", "max"),
+        high_stress_hours=("hourly_uclf_oclf", lambda x: (x < 30).sum())
+    ).reset_index()
 
-        high_stress_hours = len([value for value in uclf_values if value > 30])
+    daily["avg_uclf_oclf"] = daily["avg_uclf_oclf"].round(4)
+    daily["max_uclf_oclf"] = daily["max_uclf_oclf"].round(4)
 
+    print(daily)
+
+    for _, row in daily.iterrows():
         data = {
-            "date": date,
-            "avg_uclf_oclf": round(avg, 4),
-            "max_uclf_oclf": round(maximum, 4),
-            "high_stress_hours": high_stress_hours
+            "date": str(row["date"]),
+            "avg_uclf_oclf": float(row["avg_uclf_oclf"]),
+            "max_uclf_oclf": float(row["max_uclf_oclf"]),
+            "high_stress_hours": int(row["high_stress_hours"])
         }
-
         supabase.table("daily_outage_summary").insert(data).execute()
-        print(f"Inserted daily summary for {date}")
+        print(f"Inserted daily summary for {row['date']}")
 
 if __name__ == "__main__":
     transform_hourly_to_daily()
